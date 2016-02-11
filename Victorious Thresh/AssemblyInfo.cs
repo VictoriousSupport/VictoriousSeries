523b950c5c18f99c872be350f8f18aafab9421f1
VictoriousSupport <an.dolce@gmail.com>
2016-02-11 14:15:47 +0900

    어셈블리 명칭 변경

--- Victorious Thresh/AssemblyInfo.cs
+++ Victorious Thresh/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Jinx's Thresh")]
+[assembly: AssemblyTitle("Victorious Thresh")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Jinx's Thresh")]
+[assembly: AssemblyProduct("Victorious Thresh")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
